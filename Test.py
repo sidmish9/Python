ab2d9655e36a99b37aa69fd5aac0c2725482fc9e
Siddharth Mishra <siddhart207@gmail.com>
2020-09-04 14:57:11 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ Test.py
@@ -0,0 +1,24 @@
+# print('hello')
+name = 'James'
+i, y = 0, 2
+#new = name[i:y]
+# formatted string
+a = 'hi'
+b = 'hello'
+c = f'{a} and {b} but please say [{b}] only'
+# print(c)
+
+"""
+Checking python;s generic operations
+"""
+aa = 'Hello'
+aa.capitalize
+a = len(aa)
+
+print(aa.find('e'))  # will print the index of the character(c-sens)
+print(aa.find('elil'))  # -1 for no existent
+c = 'el' in aa
+print(c)  # to check existance of subst in str (BOOL)
+
+
+# string functions
